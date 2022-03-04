b3f6d51bf2aee4096f09078485ba74091b06c0f4
Chinmay <chinmay11.moghe@gmail.com>
2022-03-04 22:04:49 +0530

    Add todos props

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script>
-	import Todos from "./Todos.svelte";
+  import Todos from "./Todos.svelte";
+  let todos = [
+    { id: 1, name: "Create a svelte app", completed: false },
+    { id: 2, name: "Create your first svelte component", completed: true },
+    { id: 3, name: "Publish on netlify", completed: false },
+  ];
 </script>
 
-<style>
-	
-</style>
-<Todos/>
-
+<Todos {todos} />
